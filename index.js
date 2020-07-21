736c8335fce2ce80849d14822b4e9c08ffecaea2
astreminix <66201263+astreminix@users.noreply.github.com>
2020-07-21 14:07:58 +1200

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const Discord = require("discord.js");
 
 const astreminix = new Discord.Client();
 
-function sleep(ms) {
-return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 client.on('ready', async () => {
 
 astreminix.on('ready', async () => {
 
 console.log(`Logged in as ${astreminix.user.username}, ready to get this bread.`);
-
+}
+}
 });
 
 
